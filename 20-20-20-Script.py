25456a29d68c82b9b0cf23f9d4c4f7a4c1c99e8b
Luca <37956594+Rogge00@users.noreply.github.com>
2022-12-16 15:48:22 +0100

    Script Update
    Script Update

--- 20-20-20-Script.py
+++ 20-20-20-Script.py
@@ -4,10 +4,21 @@ import tkinter as tk
 # Create a function to send the notification
 def send_notification():
   # Create a new window
+  width = 600 # Width 
+  height = 300 # Height
   window = tk.Tk()
-  window.geometry("800x200")
+  window.geometry("600x300")
   window.title("Notification")
-  
+
+  # Center window
+  screen_width = window.winfo_screenwidth()  # Width of the screen
+  screen_height = window.winfo_screenheight() # Height of the screen
+
+  x = (screen_width/2) - (width/2)
+  y = (screen_height/2) - (height/2)
+
+  window.geometry('%dx%d+%d+%d' % (width, height, x, y))
+
   # Add a label to the window
   label = tk.Label(text="Stacca gli occhi dal computer per 20 secondi", font=("Helvetica", 16))
   label.pack()
@@ -24,4 +35,4 @@ input("Press enter to start the script")
 while True:
   # Send the notification every 20 minutes
   send_notification()
-  time.sleep(5)
+  time.sleep(1200)
